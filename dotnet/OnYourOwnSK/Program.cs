f9606bdb1284d91267bb63274af866ac50e24f46
Geremy Brooks <brooksgeremy@outlook.com>
2025-01-21 12:08:25 -0600

    Console app created to practice Semantic Kernels

---
+++ dotnet/OnYourOwnSK/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddAzureOpenAIChatCompletion(
+         "https://ai-gb6049ai273305106337.services.ai.azure.com/",                      // Azure OpenAI Deployment Name
+         "https://ai-gb6049ai273305106337.services.ai.azure.com/", // Azure OpenAI Endpoint
+         "AJE2E0ZTiZF5ZqZAs0wV9oQcs01OxiTCwF8QPSJdh9lSw5VA5IGGJQQJ99BAACHYHv6XJ3w3AAAAACOGsq8W");      // Azure OpenAI Key
+
+// Alternative using OpenAI
+//builder.AddOpenAIChatCompletion(
+//         "gpt-3.5-turbo",                  // OpenAI Model name
+//         "...your OpenAI API Key...");     // OpenAI API Key
+
+var kernel = builder.Build();
+
+var prompt = @"{{$input}}
+
+One line TLDR with the fewest words.";
+
+var summarize = kernel.CreateFunctionFromPrompt(prompt, executionSettings: new OpenAIPromptExecutionSettings { MaxTokens = 100 });
+
+string text1 = @"
+1st Law of Thermodynamics - Energy cannot be created or destroyed.
+2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
+3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
+
+string text2 = @"
+1. An object at rest remains at rest, and an object in motion remains in motion at constant speed and in a straight line unless acted on by an unbalanced force.
+2. The acceleration of an object depends on the mass of the object and the amount of force applied.
+3. Whenever one object exerts a force on another object, the second object exerts an equal and opposite on the first.";
+
+Console.WriteLine(await kernel.InvokeAsync(summarize, new() { ["input"] = text1 }));
+
+Console.WriteLine(await kernel.InvokeAsync(summarize, new() { ["input"] = text2 }));
+
+Console.WriteLine("Press enter to exit.");
+string userInput = Console.ReadLine();
+// Output:
+//   Energy conserved, entropy increases, zero entropy at 0K.
+//   Objects move in response to forces.
